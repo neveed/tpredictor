6296480cb0b734ce4d54e03a82ae4a10a75d0a9f
Naveed Rehman <neveed@gmail.com>
2012-06-28 12:52:34 +0500

    tpredictor first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tpredictor::Application.config.secret_token = 'e54ac30927efeaea495c0379baaadf68f9fbe57827dcec23a249060f44e90d501a9a0e576157066b2af91fbd343e23ae8a4c67928771f58fbddd65aac4f5e3d7'
